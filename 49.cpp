41da7da0148baa52cb2d18833e027efbab7dd44d
wzd <wzdwxjwxw@sjtu.edu.cn>
2022-01-25 21:10:25 +0800

    exchange dictionary

---
+++ 49.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // how to complete the code? unkonwn
+        unordered_map<string, vector<string>> mp;
+        for(auto str:strs) {
+            string temp = str;
+            sort(temp.begin(), temp.end());
+            mp[temp].push_back(str);
+        }
+        vector<vector<string>> ans;
+        for(auto it=mp.begin();it!=mp.end();it++) {
+            ans.push_back(it->second);
+        }
+        return ans;
+    }
+};
